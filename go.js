8da8c32d6aa7a7df9c3dd338566e671c32400d0d
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-10-29 18:47:01 -0700

    Update go.js

--- go.js
+++ go.js
@@ -1,4 +1,4 @@
-var c = document.getElementById("myCanvas");
+var c = document.getElementById("goCanvas");
 var ctx = c.getContext("2d");
 var canvasSize = 600
 
@@ -8,10 +8,10 @@ function start() {
   var grd = ctx.createLinearGradient(0,0,200,0);
   grd.addColorStop(0,"#D68910");
   for (i = 0; i < 20; i++) {
-	  num = i / 20
-    grd.addColorStop(num,"#D68910");
-	  grd.addColorStop(num + 0.02,"#B9770E");
-    grd.addColorStop(num + 0.04,"#D68910");
+	num = i / 20
+    	grd.addColorStop(num,"#D68910");	
+	grd.addColorStop(num + 0.02,"#B9770E");
+    	grd.addColorStop(num + 0.04,"#D68910");
   }  
   grd.addColorStop(1,"#D68910");
   // Fill with gradient
