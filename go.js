18a9a60b5f78a8905436bc4d386f536f3e187380
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-10-29 19:53:28 -0700

    Update go.js

--- go.js
+++ go.js
@@ -2,20 +2,23 @@ var c = document.getElementById("goboard");
 var ctx = c.getContext("2d");
 var canvasSize = 600
 
+function background() {
+	var num=0;
+  	// Create gradient
+  	var grd = ctx.createLinearGradient(0,0,canvasSize,0);
+  	grd.addColorStop(0,"#D68910");
+	for (i = 0; i < 20; i++) {
+		num = i / 20
+    		grd.addColorStop(num,"#D68910");	
+		grd.addColorStop(num + 0.02,"#B9770E");
+    		grd.addColorStop(num + 0.04,"#D68910");
+  	}  
+  	grd.addColorStop(1,"#D68910");
+  	// Fill with gradient
+  	ctx.fillStyle = grd;
+  	ctx.fillRect(0,0,canvasSize,canvasSize);
+}
 function start() {
-  var num=0;
-  // Create gradient
-  var grd = ctx.createLinearGradient(0,0,canvasSize,0);
-  grd.addColorStop(0,"#D68910");
-  for (i = 0; i < 20; i++) {
-	num = i / 20
-    	grd.addColorStop(num,"#D68910");	
-	grd.addColorStop(num + 0.02,"#B9770E");
-    	grd.addColorStop(num + 0.04,"#D68910");
-  }  
-  grd.addColorStop(1,"#D68910");
-  // Fill with gradient
-  ctx.fillStyle = grd;
-  ctx.fillRect(0,0,canvasSize,canvasSize);
-  alert("Page under Construction, please revisit when complete");
+  	background();
+  	alert("Page under Construction, please revisit when complete");
 }
