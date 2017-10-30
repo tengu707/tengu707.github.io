b420a4ba8a8d84388e10c27c29ab7d9b767b9c51
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-10-30 12:22:30 -0700

    Update go2.js

--- go2.js
+++ go2.js
@@ -1,5 +1,37 @@
+//programming variables
 var canvasSize = document.getElementById("canvasSize").value;
 var boardSize = document.getElementById("boardSize").value;
+var gridNum = boardSize - 1;
+var gridSize = canvasSize - 100;
+var gridX = 50;
+var gridY = 50;
+var turn = 1;
+var remove = 0;
+var button = 0;
+var skipCount = 0;
+var score = 0;
+var scoreCount = 0;
+var groupped = 0;
+var a = 0;
+var freedom = 0;
+var capturedGroups = [];
+var legal = 1;
+var capture = 0;
+var add = 0;
+var b = 0;
+var current = 0;
+var groupLines = -1;
+var duplicate = 0;
+var storeX = 0;
+var storeY = 0;
+var store = 0;
+var storage = [];
+var start = 1;
+var undos = 8;
+var minus = 3;
+var fill = "#000000";
+
+//Background creating function
 function background() {
   var c = document.getElementById("goboard");
   var ctx = c.getContext("2d");
@@ -18,6 +50,8 @@ function background() {
   ctx.fillStyle = grd;
   ctx.fillRect(0,0,canvasSize,canvasSize);
 }
+
+//function to run on start or restart
 function start() {
   var canvasSize = document.getElementById("canvasSize").value;
   var boardSize = document.getElementById("boardSize").value;
