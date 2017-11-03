e5661ed249d608525569a15fad4db9ef92375297
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-02 18:10:39 -0700

    Update go.js

--- go.js
+++ go.js
@@ -663,20 +663,20 @@ function skip() {
             skipCount++;
         }
         else {
-            //document.getElementById("skipbutton").innerHTML = "Remove all dead peices, then click here";
-	    turn = 0;
+            document.getElementById("skipbutton").innerHTML = "Remove all dead peices, then click here";
+	    //turn = 0;
             button++;
         }
     }
     else if (button === 1) {
-	//document.getElementById("skipbutton").innerHTML = "Fill in blacks territory, then click here";
-	turn = 1;
+	document.getElementById("skipbutton").innerHTML = "Fill in blacks territory, then click here";
+	//turn = 1;
         button++;
     }
     else if (button === 2) {
-	turn = 0;
-	score = black.x.length;
-	//document.getElementById("skipbutton").innerHTML = "Click here for score";
+	//turn = 0;
+	//score = black.x.length;
+	document.getElementById("skipbutton").innerHTML = "Click here for score";
 	alert("Black:" + score + "   White:" + (boardSize * boardSize - score);
     }
 	drawBoard();
