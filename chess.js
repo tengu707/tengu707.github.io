78aa0827dde55dc09721a3dacdfb6f991eff4af2
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-04 14:45:37 -0700

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,5 +1,5 @@
 //basic functions
-var c = document.getElementById("goboard");
+var c = document.getElementById("chessBoard");
 var ctx = c.getContext("2d");
 
 //A function that draws lines
@@ -20,7 +20,7 @@ function circle(centerX, centerY, diameter) {
 }
 
 //A function that draws rectangles
-function rect(x1, y1, width, height) {
+function rectangle(x1, y1, width, height) {
   ctx.beginPath();
   ctx.rect(x1, y1, width, height);
   ctx.stroke();
@@ -34,43 +34,35 @@ var fill = "#000000";
 
 //Draw the board
 function background() {
-  var num=0;
-  // Create gradient
-  var grd = ctx.createLinearGradient(0,0,canvasSize,0);
-  grd.addColorStop(0,"#D68910");
-  for (i = 0; i < 20; i++) {
-    num = i / 20;
-    grd.addColorStop(num,"#D68910");	
-    grd.addColorStop(num + 0.02,"#B9770E");
-    grd.addColorStop(num + 0.04,"#D68910");
-  }  
-  grd.addColorStop(1,"#D68910");
-  // Fill background with gradient
-  ctx.fillStyle = grd;
-  ctx.fillRect(0,0,canvasSize,canvasSize);
-  
   //Define rectangle size
-  var rectSize = (canvasSize - 100) / boardSize;
+  var rectSize = canvasSize / boardSize;
   //Create checkerboard
   fill = "#000000";
-    for (var i = 0; i < boardSize; i++) {
-      if (fill ="#ffffff") {
+  for (var i = 0; i < boardSize; i++) {
+    if (fill ==="#ffffff") {
+      fill = "#000000";
+    }
+    else {
+      fill = "#ffffff";
+    }
+    for (var j = 0; j < boardSize; j++) {
+      if (fill ==="#ffffff") {
         fill = "#000000";
       }
       else {
         fill = "#ffffff";
       }
-      for (var j = 0; j < boardSize; j++) {
-        if (fill ="#ffffff") {
-          fill = "#000000";
-        }
-        else {
-          fill = "#ffffff";
-        }
-        strokeStyle = fill;
-        rect(i * rectSize, j * rectSize, rectSize, rectSize);
-      }
+      ctx.strokeStyle = fill;
+      rectangle(i * rectSize, j * rectSize, rectSize, rectSize);
     }
+  }
+}
+
+function showCoords(event) {
+    var scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var mouseX = event.clientX - document.getElementById("chessBoard").offsetLeft + scrollLeft;
+    var mouseY = event.clientY - document.getElementById("chessBoard").offsetTop + scrollTop;
 }
 
 function start() {
