27c40064a287260584bf9a079afda9fb4f0bd425
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-17 12:54:08 -0800

    Update loader.js

--- robotstore/loader.js
+++ robotstore/loader.js
@@ -1,1 +1,10 @@
+var load;
 
+function loaded() {
+    load = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("page").style.display = "block";
+}
