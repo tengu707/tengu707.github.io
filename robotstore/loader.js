d5c371994daae11288ff4447d70300d130e8693b
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-17 16:10:45 -0800

    Update loader.js

--- robotstore/loader.js
+++ robotstore/loader.js
@@ -1,27 +1,9 @@
 var load;
-var isLoading = true;
 function loaded() {
-    //load = setTimeout(showPage, 500);
+   load = setTimeout(showPage, 500);
 }
 
 function showPage() {
-  isLoading = false;
   document.getElementById("loader").style.display = "none";
   document.getElementById("page").style.display = "block";
-}
-function loading() {
-    let i = 1;
-    while (isLoading) {
-        if (i === 1) {
-            document.getElementById("loadmessage").innerHTML = "MR ROBOT LOADING.";
-        }
-        else if (i === 2) {
-            document.getElementById("loadmessage").innerHTML = "MR ROBOT LOADING..";
-        }
-        else {
-            document.getElementById("loadmessage").innerHTML = "MR ROBOT LOADING...";
-            i = 0;
-        }
-        i++;
-    }
 }
