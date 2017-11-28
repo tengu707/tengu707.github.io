087d71c906d3d2e836f8cb651cc7519eb4b2bf91
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-27 19:00:04 -0800

    Update inventory.js

--- car/inventory.js
+++ car/inventory.js
@@ -16,8 +16,9 @@ var r1000 = new Car("r1000", "GSX R1000", "r1000.png", "This is car 5");
 
 //create string to fill inventory
 var inventoryString = "";
-alert(carIndex[i].id);
+alert(carIndex[0].id);
 for(var i = 0; i < carIndex.length; i++) {
+  alert(carIndex[i].id);
   inventoryString = inventoryString + '<article id=" ' + carIndex[i].id + ' "><div class="col-sm-6 col-md-4"><div class="thumbnail"><img id="img' + carIndex[i].id + ' " src=" ' + carIndex[i].picture + ' " width=200 height=200 alt="Picture Error"><div class="caption"><h3><strong>Name:</strong> ' + carIndex[i].name + ' </h3><p><strong>Description:</strong> ' + carIndex[i].description + ' </p></div></div></div></article>';
 }
 alert(inventoryString);
