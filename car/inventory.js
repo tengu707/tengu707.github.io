0b6bed277b21773a106da27356e22ee4ad5e8e9e
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-27 19:10:28 -0800

    Update inventory.js

--- car/inventory.js
+++ car/inventory.js
@@ -16,10 +16,11 @@ var r1000 = new Car("r1000", "GSX R1000", "r1000.png", "This is car 5");
 
 //create string to fill inventory
 var inventoryString = "";
-alert(carIndex[0].id);
-for(var i = 0; i < carIndex.length; i++) {
-  alert(carIndex[i].id);
-  inventoryString = inventoryString + '<article id=" ' + carIndex[i].id + ' "><div class="col-sm-6 col-md-4"><div class="thumbnail"><img id="img' + carIndex[i].id + ' " src=" ' + carIndex[i].picture + ' " width=200 height=200 alt="Picture Error"><div class="caption"><h3><strong>Name:</strong> ' + carIndex[i].name + ' </h3><p><strong>Description:</strong> ' + carIndex[i].description + ' </p></div></div></div></article>';
+alert(Car[1].name);
+for(var i in Car) {
+  let obj = Car[i];
+  alert(obj);
+  inventoryString = inventoryString + '<article id=" ' + obj.id + ' "><div class="col-sm-6 col-md-4"><div class="thumbnail"><img id="img' + obj.id + ' " src=" ' + obj.picture + ' " width=200 height=200 alt="Picture Error"><div class="caption"><h3><strong>Name:</strong> ' + obj.name + ' </h3><p><strong>Description:</strong> ' + obj.description + ' </p></div></div></div></article>';
 }
 alert(inventoryString);
 document.getElementById("inventory").innerHTML = inventoryString;
