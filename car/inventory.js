6b42756bae265bf1fcfd53218ae572ec184641ca
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-28 13:01:54 -0800

    Update inventory.js

--- car/inventory.js
+++ car/inventory.js
@@ -7,6 +7,8 @@ var Car = function(ID, Name, Picture, Class, MPG, Price, Description) {
   this.class = Class;
   this.mpg = MPG;
   this.price = Price;
+  this.tax = 0.08 * this.price;
+  this.total = this.tax + this.price;
   this.description = Description;
   this.show = true;
   carIndex.push(this);
@@ -23,7 +25,7 @@ var inventoryFill = function() {
   var inventoryString = "";
   for(i in carIndex) {
     if(carIndex[i].show) {
-      inventoryString = inventoryString + '<article id="' + carIndex[i].id + '"><div class="col-sm-6 col-md-4"><div class="thumbnail"><img id="img' + carIndex[i].id + '" src=" ' + carIndex[i].picture + '" width=200 height=200 alt="Picture Error"><div class="caption"><h3><strong>Model:</strong> ' + carIndex[i].name + '</h3><p><strong>Class:</strong> ' + carIndex[i].class + '</p><p><strong>MPG:</strong> ' + carIndex[i].mpg + '</p><p><strong>Description:</strong> ' + carIndex[i].description + '</p><p><strong>Price:</strong> $' + carIndex[i].price + '</p></div></div></div></article>';
+      inventoryString = inventoryString + '<article id="' + carIndex[i].id + '"><div class="col-sm-6 col-md-4"><div class="thumbnail"><img id="img' + carIndex[i].id + '" src=" ' + carIndex[i].picture + '" width=200 height=200 alt="Picture Error"><div class="caption"><h3><strong>Model:</strong> ' + carIndex[i].name + '</h3><p><strong>Class:</strong> ' + carIndex[i].class + '   <strong>MPG:</strong> ' + carIndex[i].mpg + '</p><p><strong>Description:</strong> ' + carIndex[i].description + '</p><p><strong>Price:</strong> $' + carIndex[i].price + '   <strong>Tax:</strong> $' + carIndex[i].tax + '   <strong>Total:</strong> $' + carIndex[i].total + '</p></div></div></div></article>';
     }
   }
   document.getElementById("inventory").innerHTML = inventoryString;
