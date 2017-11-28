9dcb06b1ff7d0f10cf9930c37dad7600b4b67e9f
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-28 13:06:29 -0800

    Update inventory.js

--- car/inventory.js
+++ car/inventory.js
@@ -14,11 +14,11 @@ var Car = function(ID, Name, Picture, Class, MPG, Price, Description) {
   carIndex.push(this);
 }
 
-var sierra = new Car("sierra", "Sierra", "sierra.png", "Off-Road Mini SUV", 28, 3990, "Our off-road model that can handle the most difficult road.");
-var splash = new Car("splash", "Splash", "splash.png", "City Car", 54, 5990,  "Our best commuter, with superb mpg and a comfortable ride.");
-var swift = new Car("swift", "Swift", "swift.png", "Super Mini", 36, 17990, "A stylish, powerful and economic mini.");
-var sx4 = new Car("sx4", "SX4", "sx4.png", "Compact Car", 25, 19990, "Enjoy the perfect combo of power and comfort.");
-var r1000 = new Car("r1000", "GSX R1000", "r1000.png", "Motorcycle", 35, 14990, "Our sports bike model for the street racing types.");
+var sierra = new Car("sierra", "Sierra", "sierra.png", "Off-Road Mini SUV", 28, 3900, "Our off-road model that can handle the most difficult road.");
+var splash = new Car("splash", "Splash", "splash.png", "City Car", 54, 5900,  "Our best commuter, with superb mpg and a comfortable ride.");
+var swift = new Car("swift", "Swift", "swift.png", "Super Mini", 36, 17900, "A stylish, powerful and economic mini.");
+var sx4 = new Car("sx4", "SX4", "sx4.png", "Compact Car", 25, 19900, "Enjoy the perfect combo of power and comfort.");
+var r1000 = new Car("r1000", "GSX R1000", "r1000.png", "Motorcycle", 35, 14900, "Our sports bike model for the street racing types.");
 
 //Inventory Filling Function
 var inventoryFill = function() {
