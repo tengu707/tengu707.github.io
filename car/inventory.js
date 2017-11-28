649771f08c9abda44c6be613faf45a90d66787a0
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-11-27 18:09:41 -0800

    Update inventory.js

--- car/inventory.js
+++ car/inventory.js
@@ -1,16 +1,17 @@
 carIndex = [];
 
-var Car = function(Name, Picture, Description) {
+var Car = function(ID, Name, Picture, Description) {
+  this.id = ID;
   this.name = Name;
   this.picture = "image/" + Picture;
   this.description = Description;
-  carIndex.push(this.name);
+  carIndex.push(this.ID);
 }
 
-var sierra = new Car("Sierra", "sierra.png", "This is car 1");
-var splash = new Car("Splash", "splash.png", "This is car 2");
-var swift = new Car("Swift", "swift.png", "This is car 3");
-var sx4 = new Car("SX4", "sx4.png", "This is car 4");
-var r1000 = new Car("GSX R1000", "r1000.png", "This is car 5");
+var sierra = new Car("sierra", "Sierra", "sierra.png", "This is car 1");
+var splash = new Car("splash", "Splash", "splash.png", "This is car 2");
+var swift = new Car("swift", "Swift", "swift.png", "This is car 3");
+var sx4 = new Car("sx4", "SX4", "sx4.png", "This is car 4");
+var r1000 = new Car("r1000", "GSX R1000", "r1000.png", "This is car 5");
 
-alert(carIndex[3]);
+alert(swift.picture);
