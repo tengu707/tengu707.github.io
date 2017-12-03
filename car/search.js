ffe9dd2d9cb6292c02811c9e2c65bf221e0b9386
tengu707 <33167542+tengu707@users.noreply.github.com>
2017-12-03 15:14:22 -0800

    Update search.js

--- car/search.js
+++ car/search.js
@@ -1,27 +1,19 @@
 //search filter function
   $('#searchbox').change(function() {
-    let search = document.getElementById("searchbox").value;
-    if (search.match(/dog|R.E.T|ret|E.T.|ET|pet/i) ) {
-      $('#robo1').removeClass('invisible');
-      $('#robo2').addClass('invisible');
-      $('#robo3').addClass('invisible');
-      
-    }
-    else if (search.match(/butler|martini|chore|housework/i) ) {
-      $('#robo2').removeClass('invisible');
-      $('#robo1').addClass('invisible');
-      $('#robo3').addClass('invisible');
-    }
-    else if (search.match(/mega|man|freind|companion/i) ) {
-      $('#robo3').removeClass('invisible');
-      $('#robo1').addClass('invisible');
-      $('#robo2').addClass('invisible');
-    }
-    else {
-      for (let i = 0; i < highlightAreas.length; i++) {
-        $('#robo1').removeClass('invisible');
-        $('#robo2').removeClass('invisible');
-        $('#robo3').removeClass('invisible');
-      }
+    // Declare variables
+    var input, filter, ul, li, h3, i;
+    input = document.getElementById('searchbox');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("inventory");
+    li = ul.getElementsByTagName('article');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        h3 = li[i].getElementsByTagName("h3")[0];
+        if (h3.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
     }
   });
