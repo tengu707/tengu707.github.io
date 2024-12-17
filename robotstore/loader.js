7835902a24ab95ae61dfdb89a1ff8d97de0171e2
tengu707 <33167542+tengu707@users.noreply.github.com>
2024-12-16 17:18:09 -0800

    Delete robotstore directory

--- robotstore/loader.js
+++
@@ -1,9 +0,0 @@
-var load;
-function loaded() {
-   load = setTimeout(showPage, 500);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("page").style.display = "block";
-}
