fac6df136044565978d12adf7f3e1dd451c5594a
tengu707 <33167542+tengu707@users.noreply.github.com>
2024-12-16 17:17:39 -0800

    Delete SQL Project directory

--- SQL Project/part1.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM customers
-INNER JOIN invoices USING (CustomerId)
-INNER JOIN invoice_items USING (InvoiceId)
-WHERE TrackId IN (8, 21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 34, 41, 42, 45, 47, 52, 53);
